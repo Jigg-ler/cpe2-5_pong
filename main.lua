a1d99bd5b253de3fdadbc41ed4648fd77121b18c
Jigg-ler <angelolobabia@gmail.com>
2020-11-05 14:27:09 +0800

    added a static rectangle lol

--- main.lua
+++ main.lua
@@ -7,9 +7,13 @@
 -- a more retro aesthetic
 push = require 'push'
 
+--class is a library that allows us to implement classes similar to
+--other languages
+Class = require 'class'
+
 --the actual size of the window/program
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1080
+WINDOW_HEIGHT = 600
 
 --can be adjusted for the amount of pixels shown virtually
 VIRTUAL_WIDTH = 432
@@ -23,7 +27,7 @@ function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
 
     --stores the font settings to the object font32
-    font32 = love.graphics.newFont('font2.ttf', 32)
+    font32 = love.graphics.newFont('font.ttf', 32)
     love.graphics.setFont(font32)
     
     -- initialize our virtual resolution, which will be rendered within our
@@ -62,7 +66,10 @@ function love.draw()
 
     -- condensed onto one line from last example
     -- note we are now using virtual width and height now for text placement
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 6 - 16, VIRTUAL_WIDTH, 'center')
+
+    --(drawmode, x, y, width, height)
+    love.graphics.rectangle('fill', 10, VIRTUAL_HEIGHT / 2, 5, 30)
 
     -- end rendering at virtual resolution
     push:apply('end')
