2740f69f743f9d33a5a57cdff0a694d19eac4a81
Jigg-ler <angelolobabia@gmail.com>
2020-11-07 20:00:51 +0800

    added game states, cleaned comments, implemented scoring

--- main.lua
+++ main.lua
@@ -11,8 +11,10 @@ push = require 'push'
 --other languages
 Class = require 'class'
 
+--Ball class
 require 'Ball'
 
+--Paddle class
 require 'Paddle'
 
 --the actual size of the window/program
@@ -35,23 +37,22 @@ local snd2 = love.audio.newSource("assets/sfx/hit.wav", "static")
 local snd3 = love.audio.newSource("assets/sfx/hit1.wav", "static")
 local snd4 = love.audio.newSource("assets/sfx/start.wav", "static")
 local snd5 = love.audio.newSource("assets/sfx/menu.wav", "stream")
+local snd6 = love.audio.newSource("assets/sfx/winner.wav", "static")
 
---[[
-    Runs when the game first starts up, only once; used to initialize the game.
-]]
+--Runs when the game first starts up, only once; used to initialize the game.
 function love.load()
     --reduces blur from the edges of the font due to the virtual retro-ing
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    --sets the title of the window
     love.window.setTitle('BSCpE 2-5 Pong')
 
     -- "seed" the RNG so that calls to random are always random
     -- use the current time, since that will vary on startup every time
     math.randomseed(os.time())
     
-    --stores the font settings to the object font32
-    font32 = love.graphics.newFont('font.ttf', 8)
-    love.graphics.setFont(font32)
+    --stores the font settings to their respective variables
+    mainFont = love.graphics.newFont('font.ttf', 8)
 
     --initializes score variables
     player1Score = 0
@@ -60,6 +61,9 @@ function love.load()
     --initializes the two paddles and their properties (x, y, width, height)
     player1 = Paddle(7, VIRTUAL_HEIGHT / 2 - 15, 7, 31)
     player2 = Paddle(VIRTUAL_WIDTH - 14, VIRTUAL_HEIGHT / 2 - 15, 7, 31)
+
+    --velocity and position variables for our ball when play starts
+    ball = Ball(VIRTUAL_WIDTH / 2 -2, VIRTUAL_HEIGHT / 2 - 2 , 6, 6)
     
     -- plays the start sfx
     love.audio.play(snd4)
@@ -71,9 +75,6 @@ function love.load()
         resizable = false,
         vsync = true
     })
-
-    --velocity and position variables for our ball when play starts
-    ball = Ball(VIRTUAL_WIDTH / 2 -2, VIRTUAL_HEIGHT / 2 - 2 , 6, 6)
     
     gameState = 'start'
 end
@@ -83,7 +84,8 @@ end
 --e.g. player movement
 function love.update(dt)
     if gameState == 'play' then
-        --player1
+
+        --player1 movement
         if love.keyboard.isDown('w') then
             player1.dy = -PADDLE_SPEED
         elseif love.keyboard.isDown('s') then
@@ -92,7 +94,7 @@ function love.update(dt)
             player1.dy = 0
         end
 
-        --player2
+        --player2 movement
         if love.keyboard.isDown('up') then
             player2.dy = -PADDLE_SPEED
         elseif love.keyboard.isDown('down') then
@@ -104,7 +106,7 @@ function love.update(dt)
         ball:update(dt)       
         if ball:collides(player1) then
             ball.dx = -ball.dx * 1.03
-            ball.x = player1.x + 5
+            ball.x = player1.x + ball.width
             love.audio.play(snd2)
 
             if ball.dy < 0 then
@@ -115,7 +117,7 @@ function love.update(dt)
         end
         if ball:collides(player2) then
             ball.dx = -ball.dx * 1.03
-            ball.x = player2.x - 4
+            ball.x = player2.x - ball.width
             love.audio.play(snd2)
 
             if ball.dy < 0 then
@@ -133,7 +135,7 @@ function love.update(dt)
         end
 
         if ball.y >= VIRTUAL_HEIGHT then
-            ball.y = VIRTUAL_HEIGHT - 4
+            ball.y = VIRTUAL_HEIGHT - ball.height
             ball.dy = -ball.dy
             love.audio.play(snd3)
         end
@@ -143,7 +145,18 @@ function love.update(dt)
             servingPlayer = 1
             player2Score = player2Score + 1
             ball:reset()
-            gameState = 'serve'
+            gameState = 'service'
+
+            if player2Score == 2 then
+                --stops bg music
+                snd5:stop()
+                
+                --plays winning sound
+                love.audio.play(snd6)
+                
+                gameState = 'end'
+                winner = 2
+            end
         end
 
         if ball.x > VIRTUAL_WIDTH then
@@ -151,12 +164,30 @@ function love.update(dt)
             servingPlayer = 2
             player1Score = player1Score + 1
             ball:reset()
-            gameState = 'serve'
+            gameState = 'service'
+            
+            if player1Score == 2 then
+                --stops bg music
+                snd5:stop()
+
+                --plays winning sound
+                love.audio.play(snd6)
+                
+                gameState = 'end'
+                winner = 1
+            end
         end
+
         player1:update(dt)
         player2:update(dt)
-    end
 
+    elseif gameState == 'service' then
+        if servingPlayer == 2 then
+            ball.dx = math.random(100, 150)
+        else
+            ball.dx = -math.random(100,150)
+        end
+    end
 end
 
 --checks per frame if a key is certain key is pressed
@@ -169,20 +200,31 @@ function love.keypressed(key)
     --during play mode, the ball will move in a random direction
     elseif key == 'enter' or key == 'return' then
         love.audio.play(snd3)
-        snd5:setLooping(true)
-        snd5:stop()
-        snd5:play()
+
         if gameState == 'start' then
+            --bg music loop
+            snd5:setLooping(true)
+            snd5:stop()
+            snd5:play()
+
+            gameState = 'service'
+
+        elseif gameState == 'service' then
             gameState = 'play'
 
-        else
-            gameState = 'start'
+        elseif gameState == 'end' then
+            --resets score
+            player1Score = 0
+            player2Score = 0
 
-            ball:reset()
+            --plays the bg music again
+            snd5:play()
+
+            --starts the game over again
+            gameState = 'service'            
+
         end
     end
-    -- TO IMPLEMENT -key for enter to start the game 
-
 end
 
 --[[
@@ -196,10 +238,37 @@ function love.draw()
     -- loads background asset
     love.graphics.draw(background, 0, 0)
 
-    -- condensed onto one line from last example
-    -- note we are now using virtual width and height now for text placement
-    love.graphics.printf('HELLO PONG!', 0, VIRTUAL_HEIGHT / 6 - 28, VIRTUAL_WIDTH, 'center')
+    --sets the font
+    love.graphics.setFont(mainFont)
 
+    if gameState == 'start' then
+        -- condensed onto one line from last example
+        -- note we are now using virtual width and height now for text placement
+        love.graphics.printf('START PONG!', 0, VIRTUAL_HEIGHT / 6 - 28, VIRTUAL_WIDTH, 'center')
+    
+    elseif gameState == 'service' then
+        love.graphics.printf("SERVICE", 0, VIRTUAL_HEIGHT / 6 - 28, VIRTUAL_WIDTH, 'center')
+
+        --love.graphics.setFont(subFont)
+        if servingPlayer == 2 then
+            love.graphics.printf("PLAYER 2'S SERVICE", 0, VIRTUAL_HEIGHT / 4 + 4, VIRTUAL_WIDTH, 'center')
+        else
+            love.graphics.printf("PLAYER 1'S SERVICE", 0, VIRTUAL_HEIGHT / 4 + 4, VIRTUAL_WIDTH, 'center')
+        end
+    
+    elseif gameState == 'play' then
+        love.graphics.printf('PLAY', 0, VIRTUAL_HEIGHT / 6 - 28, VIRTUAL_WIDTH, 'center')
+
+    elseif gameState == 'end' then
+        love.graphics.printf("PLAYER " .. tostring(winner) .. "'s WIN!", 0, VIRTUAL_HEIGHT / 6 - 28, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf("PRESS ENTER TO START AGAIN", 0, VIRTUAL_HEIGHT / 4 + 4, VIRTUAL_WIDTH, 'center')
+    end
+
+    --player1 score
+    love.graphics.printf(tostring(player1Score), 0, VIRTUAL_HEIGHT / 6 - 28, VIRTUAL_WIDTH - 150, 'center')
+    --player2 score
+    love.graphics.printf(tostring(player2Score), 0, VIRTUAL_HEIGHT / 6 - 28, VIRTUAL_WIDTH + 150, 'center')
+    
     --renders the paddles virtually
     player1:render()
     player2:render()
